6216dbbdb62f707211a67d6558edd192a7b0032a
Kevin <kjplaye@gmail.com>
2016-09-17 13:32:25 -0400

    omit all black fractal

--- random_mandel.py
+++ random_mandel.py
@@ -14,8 +14,12 @@ if len(sys.argv) < 3:
 
 width = int(sys.argv[1])
 height = int(sys.argv[2])
+
+x_max = 0
+while x_max == 0:
+    x = numpy.zeros([height,width,4],dtype = numpy.uint8)
+    _random_mandel.main_draw(x.ctypes.get_as_parameter(),ctypes.c_int32(width),ctypes.c_int32(height))
+    x_max = x.max()
     
-x = numpy.zeros([height,width,4],dtype = numpy.uint8)
-_random_mandel.main_draw(x.ctypes.get_as_parameter(),ctypes.c_int32(width),ctypes.c_int32(height))
 X = Image.fromarray(x[:,:,:3])
 X.save(my_path + '/random_mandel.jpg')
